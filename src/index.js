fb48a1c0755fc341e8fca367d7eee5d3d0092aa3
DimiVaksman <vde92@hotmail.com>
2023-03-19 21:45:46 +0100

    hw11

--- src/index.js
+++ src/index.js
@@ -8,19 +8,22 @@ import axios, {isCancel, AxiosError} from 'axios';
 const URL = 'https://pixabay.com/api/'
 const KEY = '34475596-b269349df24b9ffe76cf99f2a'
 
-let items = [];;
+let items = [];
 let page = 1;
-let query = 'cats';
+let query = '';
 let per_page = 40;
+let pages = 0
 
 // const PARAM = '&image_type=photo&orientation=horizontal&safesearch=true&'
 const param = new URLSearchParams({ hits:'webformatURL,largeImageURL,tags,likes,views,comments,downloads'
 });
 const refs = {
     galleryList: document.querySelector('.gallery'),
-    buttonsearchPhoto: document.querySelector('button'),
+    // buttonsearchPhoto: document.querySelector('button'),
+    inputEl: document.querySelector('input'),
+    form: document.querySelector('form'),
+    loadMore: document.querySelector('.load-more')
 }
-console.log(refs.buttonsearchPhoto);console.log(refs.galleryList)
 
 // axios
 // .get(`${URL}?key=${KEY}q=yellow+flowers&${PARAM}`)
@@ -28,6 +31,8 @@ console.log(refs.buttonsearchPhoto);console.log(refs.galleryList)
 // .catch(error => {console.log(error)})
 
 
+
+    
 async function getPhoto() {
     try {
       const response = await axios.get(`${URL}?key=${KEY}&q=${query}&page=${page}&per_page=${per_page}`, {
@@ -40,7 +45,6 @@ async function getPhoto() {
       const data = await response.data
       const hits = await data.hits
       console.log(data)
-      
       insertContent(data.hits)
       if(data.total === 0){
         Notify.warning('Sorry, there are no images matching your search query. Please try again.');
@@ -59,29 +63,55 @@ async function getPhoto() {
 
 
   createImgGalerry = (item) => `
+  <a href="${item.largeImageURL} class="Large-img"">
   <div class="photo-card">
-  <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" width="420" height="340"/>
+  <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" width="280" height="260"/>
   <div class="info">
     <p class="info-item">
-      <b>${item.likes}</b>
+      <b>Likes: ${item.likes}</b>
     </p>
     <p class="info-item">
-      <b>${item.views}</b>
+      <b>Views: ${item.views}</b>
     </p>
     <p class="info-item">
-      <b>${item.comments}</b>
+      <b>Comments: ${item.comments}</b>
     </p>
     <p class="info-item">
-      <b>${item.downloads}</b>
+      <b>Downloads: ${item.downloads}</b>
     </p>
   </div>
-</div>`
+</div></a>`
+
 
 const generateContent = (array) => array?.reduce((acc,item) => acc + createImgGalerry(item), "");
+
 
 const insertContent = (array) => {
     const result = generateContent(array);
     refs.galleryList.insertAdjacentHTML("beforeend", result);
 }
 
-getPhoto()
+function refresh() {
+    refs.galleryList.innerHTML = '';    
+}
+
+
+
+
+refs.form.addEventListener('submit' , onSearchPhoto )
+
+
+function onSearchPhoto(e) {
+    e.preventDefault()  
+
+    const searchEl = refs.inputEl.value.trim().toLowerCase();
+    query = searchEl  
+    getPhoto() 
+    refresh()
+}
+
+
+
+
+
+//   refs.loadMore.addEventListener('click' , handleLoadMore)
