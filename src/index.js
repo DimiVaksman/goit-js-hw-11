1b9483676c02f54f9d6ba724d14f8bf647bf16e3
DimiVaksman <vde92@hotmail.com>
2023-03-22 21:41:50 +0100

    hw11

--- src/index.js
+++ src/index.js
@@ -1,37 +1,38 @@
 import './css/styles.css';
-
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
 import {Notify} from 'notiflix';
-
 import axios, {isCancel, AxiosError} from 'axios';
-
 
 
 const URL = 'https://pixabay.com/api/'
 const KEY = '34475596-b269349df24b9ffe76cf99f2a'
 
-let items = [];
+let hits = [];
 let page = 1;
 let query = '';
 let per_page = 40;
 let totalHits = 0;
+let lastItem = '';
 
-const param = new URLSearchParams({ hits:'webformatURL,largeImageURL,tags,likes,views,comments,downloads'
-});
 const refs = {
     galleryList: document.querySelector('.gallery'),
     inputEl: document.querySelector('input'),
     form: document.querySelector('form'),
     loadMore: document.querySelector('.load-more'),
-    galleryContainer: document.querySelector('.gallery')
+    galleryContainer: document.querySelector('.gallery'),
 }
 
 
 
-    
+
+refs.loadMore.classList.add('is-hidden'); //В початковому стані кнопка повинна бути прихована.
+
+
+scroll()
+
 async function getPhoto() {
+
     try {
       const response = await axios.get(`${URL}?key=${KEY}&q=${query}&page=${page}&per_page=${per_page}`, {
         params: {
@@ -44,10 +45,21 @@ async function getPhoto() {
       const hits = await data.hits
       console.log(data)
       insertContent(data.hits)
+    
+const galleryLightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+galleryLightbox.refresh()
       if(data.total === 0){
         Notify.warning('Sorry, there are no images matching your search query. Please try again.');
       }
-      else{
+      // if(onSearchPhoto){
+      //   refs.loadMore.classList.remove('is-hidden');
+      // }
+
+      if(onSearchPhoto){
          Notify.success(`Hooray! We found ${data.totalHits} images`);}
 
     } catch (error) {
@@ -69,16 +81,16 @@ const createImgGalerry = (item) => `
 <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" width="280" height="260"/>
 <div class="info">
   <p class="info-item">
-    <b>Likes: ${item.likes}</b>
+    <b>Likes:${item.likes}</b>
   </p>
   <p class="info-item">
-    <b>Views: ${item.views}</b>
+    <b>Views:${item.views}</b>
   </p>
   <p class="info-item">
-    <b>Comments: ${item.comments}</b>
+    <b>Comments:${item.comments}</b>
   </p>
   <p class="info-item">
-    <b>Downloads: ${item.downloads}</b>
+    <b>Downloads:${item.downloads}</b>
   </p>
 </div>
 </div>
@@ -118,35 +130,55 @@ function onSearchPhoto(e) {
     refresh()
 }
 
-// const handleLoadMore = e => {
+
+
+// const handleLoadMore = e => {   // подгрузка контента
 //   page++
 //   getPhoto() 
+
 // }
 
-//   refs.loadMore.addEventListener('click' , handleLoadMore)
+ 
 
 
 
-function checkPosition() {
-  const height = document.body.offsetHeight
-  const screenHeight = window.innerHeight
-  const scrolled = window.scrollY
-  const threshold = height - screenHeight / 4
-  const position = scrolled + screenHeight
 
-  if (position >= threshold) {
-    page++
-    getPhoto()
-    
+  function checkPosition() {
+    const height = document.body.offsetHeight
+    const screenHeight = window.innerHeight
+    const scrolled = window.scrollY
+    const threshold = height - screenHeight / 4
+    const position = scrolled + screenHeight
+  
+    if (position >= threshold) {
+      page++
+      getPhoto() 
+  
+    }
   }
-}
-
-  window.addEventListener('scroll', checkPosition)
-  window.addEventListener('resize', checkPosition)
-
-
-
-
+  
+  function throttle(callee, timeout) {
+    let timer = null
+  
+    return function perform(...args) {
+      if (timer) return
+  
+      timer = setTimeout(() => {
+        callee(...args)
+  
+        clearTimeout(timer)
+        timer = null
+      }, timeout)
+    }
+  }
+  
+  
+  function scroll() {
+    window.addEventListener('scroll', throttle(checkPosition,500))
+    window.addEventListener('resize', throttle(checkPosition,500))
+  }
+  
+  // refs.loadMore.addEventListener('click' , handleLoadMore)
 
 
 
@@ -154,9 +186,6 @@ function checkPosition() {
 
 //   refs.galleryContainer.addEventListener('click' , onClickImg)
 
-//   new SimpleLightbox('.gallery a', {
-//     captionDelay: 250,
-//  });
 
 //   function onClickImg(e) {
 //     e.preventDefault();
@@ -167,3 +196,5 @@ function checkPosition() {
 
 
 //   }
+
+
